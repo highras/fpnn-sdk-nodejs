955078b00568f90f9b067343af31fd99bd03ef17
zhao.dx <zhaod1025@gmail.com>
2018-05-03 19:19:12 +0800

    fix

--- test/test-fpnn.js
+++ test/test-fpnn.js
@@ -8,20 +8,26 @@ const FPClient = require('../src/FPClient');
 
 let client = new FPClient({ host: '35.167.185.139', port: 13013, autoReconnect: true, connectionTimeout: 10 * 1000 });
 
-fs.readFile(path.resolve(__dirname, '../key/test-secp256k1-compressed-public.key'), function(err, data){
-    if (err) throw err;
+fs.readFile(path.resolve(__dirname, '../key/test-secp256k1-compressed-public.key'), function(err, data) {
+
+    if (err) {
+
+        throw err;
+    }
 
     client.encryptor('secp256k1', data, false, 128);
-    client.connect(function(fpEncryptor){
+    client.connect(function(fpEncryptor) {
+
         return msgpack.encode({ 
             publicKey:fpEncryptor.pubKey, 
             streamMode:fpEncryptor.streamMode, 
             bits:fpEncryptor.strength 
-        })
+        });
     });
 });
 
-client.on('connect', function(){
+client.on('connect', function() {
+
     let options = {
         flag: 1,
         method: 'httpDemo',
@@ -33,12 +39,14 @@ client.on('connect', function(){
     //     payload: JSON.stringify({pid:10, sign:'', salt:1111111111111, from:122, to:123, mid:345, mtype:20, msg:'sss', attrs:''}),
     // };
 
-    client.sendQuest(options, function(data){
+    client.sendQuest(options, function(data) {
+
         console.log(data);
         // console.log(msgpack.decode(data.payload));
     }, 10 * 1000);
 });
 
-client.on('error', function(err){
+client.on('error', function(err) {
+
     console.error(err);
 });
